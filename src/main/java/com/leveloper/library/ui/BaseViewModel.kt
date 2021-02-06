8172e31e8403714627b774b4d5d97a2884d9e61d
정상현 <tkdgusl94@mailplug.co.kr>
2021-02-06 22:09:42 +0900

    BaseViewModel 수정

--- src/main/java/com/leveloper/library/ui/BaseViewModel.kt
+++ src/main/java/com/leveloper/library/ui/BaseViewModel.kt
@@ -1,28 +1,23 @@
 package com.leveloper.library.ui
 
-import android.app.Application
 import androidx.annotation.StringRes
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import java.lang.ref.WeakReference
 
-open class BaseViewModel<N>(application: Application) : AndroidViewModel(application) {
+open class BaseViewModel<N> : ViewModel() {
 
     private var navigator: WeakReference<N>? = null
 
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> = _isLoading
+    private val _isLoading = NonNullMutableLiveData(false)
+    val isLoading: NonNullLiveData<Boolean> = _isLoading
 
     private val _toast = MutableLiveData<String>()
     val toast: LiveData<String> = _toast
 
     private val _toastRes = MutableLiveData<Int>()
     val toastRes: LiveData<Int> = _toastRes
-
-    init {
-        _isLoading.value = false
-    }
 
     fun setNavigator(navigator: N) {
         this.navigator = WeakReference(navigator)
